51b9a51a0f411b22ce68405c415277c29cbaa5b1
James <bruce.nunk@gmail.com>
2019-10-11 21:48:49 +1100

    changed run test script to deploy istio to kind cluster before test

---
+++ install-istio.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+_namespace="istio-system"
+_root="./helm/istio-1.3.2"
+
+kubectl create namespace ${_namespace}
+
+
+# init.
+helm template "${_root}/install/kubernetes/helm/istio-init" \
+  --name istio-init \
+  --namespace ${_namespace} \
+  | kubectl apply -f -
+
+kubectl wait job.batch \
+  --all \
+  --for=condition=complete \
+  --namespace ${_namespace} \
+  --timeout=2m
+
+
+# delete completed pods so I can wait for all main pods to become ready in the next step.
+kubectl delete pods \
+  --all \
+  --namespace ${_namespace}
+
+
+# main.
+helm template "${_root}/install/kubernetes/helm/istio" \
+  --name istio \
+  --namespace ${_namespace} \
+  --values "${_root}/install/kubernetes/helm/istio/values-istio-minimal.yaml" \
+  | kubectl apply -f -
+
+kubectl wait pod \
+  --all \
+  --for=condition=ready \
+  --namespace ${_namespace} \
+  --timeout=2m
--- run-test.sh
+++ run-test.sh
@@ -4,6 +4,12 @@ NAME="test"
 set -euo pipefail
 trap "kind delete cluster --name ${NAME}" EXIT
 
-kind create cluster --name ${NAME} --config cluster-config.yaml
-go test -v ./...
+kind create cluster --name ${NAME}
+export KUBECONFIG="$(kind get kubeconfig-path --name="${NAME}")"
 
+
+./install-istio.sh
+
+
+_disable_cache="-count=1"
+go test -v ${_disable_cache} ./...
