919741336e2bb15377983be30650f81bb4c76941
James Lee <jlee@tyro.com>
2019-12-19 16:51:28 +1100

    added kind config to expose ingress gateway http(s) ports as node ports. added test script to curl ingress gateway. added istio-injection labels to sample namespace.

---
+++ k8s/sample/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+RQ=${BASH_ARGV[0]}
+H=$(echo ${RQ} | awk -F/ '{print $3}')
+
+curl -k \
+  --connect-to ${H}:80:127.0.0.1:8080 \
+  --connect-to ${H}:443:127.0.0.1:8443 \
+  "$@"
--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-GO111MODULE="on" go get sigs.k8s.io/kind@v0.6.1
-
+#CORECTL_VERSION="v1.0.0-alpha.28"
 CLUSTER_NAME="kind"
 K8S_VERSION="v1.14.9"
 #KCFG=$(mktemp)
 
+GO111MODULE="on" go get sigs.k8s.io/kind@v0.6.1
+
 #trap "kind delete cluster --name ${CLUSTER_NAME}" EXIT
 kind delete cluster --name ${CLUSTER_NAME}
-kind create cluster --name ${CLUSTER_NAME} --image kindest/node:${K8S_VERSION}
+kind create cluster --name ${CLUSTER_NAME} --config kind-config.yaml --image kindest/node:${K8S_VERSION}
+#kind load docker-image 519920317464.dkr.ecr.ap-southeast-2.amazonaws.com/corectl:${CORECTL_VERSION}
 #kind get kubeconfig --name ${CLUSTER_NAME} > $KCFG
 
 kubectl create ns istio-system
