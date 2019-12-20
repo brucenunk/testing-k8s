d6db8bb2282b245c7453c89669c2a099e9e23985
James Lee <jlee@tyro.com>
2019-12-20 15:04:33 +1100

    removed knative monitoring. replaced httpbin with http echo.

--- install-knative.sh
+++ install-knative.sh
@@ -4,10 +4,8 @@ set -euxo pipefail
 VERSION="v0.11.0"
 
 kubectl apply \
-  -f https://github.com/knative/serving/releases/download/${VERSION}/monitoring.yaml \
   -f https://github.com/knative/serving/releases/download/${VERSION}/serving.yaml \
   -l knative.dev/crd-install=true
 
 kubectl apply \
-  -f https://github.com/knative/serving/releases/download/${VERSION}/monitoring.yaml \
   -f https://github.com/knative/serving/releases/download/${VERSION}/serving.yaml
