77e3b99cf8794b77a74feb6b679f0dc3d4478141
James Lee <jlee@tyro.com>
2020-03-05 13:47:43 +1100

    working argocd

--- setup-argocd.sh
+++ setup-argocd.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-kubectl apply -f ./k8s/argocd/ns.yaml
-kubectl apply -n argocd -f ./k8s/argocd/.
+NAMESPACE=argocd
 
+kubectl apply -f ./k8s/argocd/ns.yaml
+kubectl apply -n ${NAMESPACE} -f ./k8s/argocd/.
+
+kubectl wait pods \
+  --all \
+  --for=condition=ready \
+  --namespace ${NAMESPACE} \
+  --timeout=10m
+
+echo $(kubectl -n ${NAMESPACE} get pods -lapp.kubernetes.io/name=argocd-server -o json | jq -r '.items[].metadata.name') > PASSWD
