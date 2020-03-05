8e61882205761e5855cb8bbaadb41b86af63b0c6
James Lee <jlee@tyro.com>
2020-03-05 14:34:26 +1100

    re-enabled pilot protocol sniffing (default) and changed argocd-repo-server port name to be tcp-server

--- setup-argocd.sh
+++ setup-argocd.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# add corectl because its deployed into argocd-server pod.
+kind load docker-image 519920317464.dkr.ecr.ap-southeast-2.amazonaws.com/corectl:v1.0.0-alpha.28
+
 NAMESPACE=argocd
 
 kubectl apply -f ./k8s/argocd/ns.yaml
